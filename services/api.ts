e35b497d2cedd93238a8889d7abaceaf1def17b3
Evgenii Aleksandrov <evgeniy.alexandrov1994@gmail.com>
2022-12-20 01:06:12 +0100

    added dockerfile and properties

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,17 @@
 import axios from "axios"
 import {BookmarksResponse} from "./models";
+import getConfig from "next/config";
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig()
 
 
-const API_BASE_URL = "http://localhost:8080"
+const getApiUrl = () => {
+    return serverRuntimeConfig.API_BASE_URL || publicRuntimeConfig.API_BASE_URL;
+}
 
 export const fetchBookmarks = async (page: number, query: string): Promise<BookmarksResponse> => {
-    let url = `${API_BASE_URL}/api/bookmarks?page=${page}`;
+    console.log("serverRuntimeConfig", serverRuntimeConfig)
+    console.log("publicRuntimeConfig", publicRuntimeConfig)
+    let url = `${getApiUrl()}/api/bookmarks?page=${page}`;
     if (query){
         url += `&query=${query}`
     }
@@ -14,6 +20,8 @@ export const fetchBookmarks = async (page: number, query: string): Promise<Bookm
 }
 
 export const saveBookmark = async (bookmark: {title:string, url:string}) => {
-    const res = await axios.post<BookmarksResponse>(`${API_BASE_URL}/api/bookmarks`,bookmark)
+    console.log("serverRuntimeConfig", serverRuntimeConfig)
+    console.log("publicRuntimeConfig", publicRuntimeConfig)
+    const res = await axios.post<BookmarksResponse>(`${getApiUrl()}/api/bookmarks`,bookmark)
     return res.data
 }
